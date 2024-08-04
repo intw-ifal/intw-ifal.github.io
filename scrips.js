d7f59452553c231f95cfebb6c1ed8d574221cdeb
intw-ifal <164233137+intw-ifal@users.noreply.github.com>
2024-08-04 08:33:17 -0300

    AddFuncaoPreferenciaLinguagem

--- scrips.js
+++ scrips.js
@@ -1,4 +1,27 @@
 function Bemvindo(nome) {
-    return alert('Bem-vindo, saudações '+nome);
+    return alert('Bem-vindo, saudações '+nome);    
+}
+
+function PreferenciaLinguagem(nome, linguagem) {
+    var nome, lp='';
+    if (nome==''){
+        alert('É preciso inserir algum nome!');
+        document.getElementById(nome).focus();
+        return false;
+    }
+    switch(linguagem) {
+        case '1':
+          lp = 'C';
+          break;
+        case '2':
+            lp = 'Python';
+          break;
+        case '3':
+            lp = 'JS';
+          break;
+        default:
+        lp = 'NENHUMA!'
+      }
+      return alert('Linguagem selecionada por '+nome+' foi '+lp);
 }
 
